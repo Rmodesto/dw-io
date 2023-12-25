a91f279897dcaced9fefe8b0e4793fd951b25e53
Rafael Modesto <1rafaelmodesto@gmail.com>
2023-12-24 22:25:42 -0500

    dashoard set up

--- app/dashboard/Sidebar.tsx
+++ app/dashboard/Sidebar.tsx
@@ -1,28 +1,62 @@
-// components/Sidebar.tsx
+'use client';
+
+import clsx from 'clsx';
 import Link from 'next/link';
-import React from 'react';
+import { usePathname } from 'next/navigation';
 
-const Sidebar: React.FC = () => (
-  <nav className="w-1/5 bg-purple-100 items-center text-lg text-black-500 p-4 border-r border-aqua-200 flex flex-col justify-between h-screen">
-    <div className="space-y-2">
-      {/* Top links */}
-      <Link href="/interpret">Interpret</Link>
-      <Link href="/dreams" className="cursor-pointer block">
-        Dreams
-      </Link>
-    </div>
-    <div className="mt-auto space-y-2">
-      {/* Bottom links */}
-      <Link href="/billing" className="cursor-pointer block">
-        Billing
-      </Link>
-      <Link href="/settings" className="cursor-pointer block">
-        Settings
-      </Link>
-      {/* Logout button */}
-      <button className=" w-full text-left">Logout</button>
-    </div>
-  </nav>
-);
+const topLinks = [
+  { name: 'Interpret', href: '/' },
+  { name: 'Dreams', href: '/dreams' },
+];
 
-export default Sidebar;
+const bottomLinks = [
+  { name: 'Billing', href: '/billing' },
+  { name: 'Settings', href: '/settings' },
+];
+
+export default function Sidebar() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="w-1/5 bg-purple-100 text-lg text-black-500 p-4 border-r border-aqua-200 flex flex-col justify-between h-screen">
+      <div className="space-y-2">
+        {/* Top links */}
+        {topLinks.map((link) => (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center  gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-aqua-200 hover:text-white-500 md:flex-none md:justify-center md:p-2 md:px-3',
+              {
+                'bg-aqua-100 text-white-500': pathname === link.href,
+              }
+            )}
+          >
+            {link.name}
+          </Link>
+        ))}
+      </div>
+      <div className="mt-auto space-y-2">
+        {/* Bottom links */}
+        {bottomLinks.map((link) => (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-aqua-100 hover:text-white-500 md:flex-none md:justify-center md:p-2 md:px-3',
+              {
+                'bg-aqua-100 text-white-500': pathname === link.href,
+              }
+            )}
+          >
+            {link.name}
+          </Link>
+        ))}
+        {/* Logout button */}
+        <button className="w-full text-left px-3 py-2 text-black-500 rounded-md hover:bg-purple-200">
+          Logout
+        </button>
+      </div>
+    </nav>
+  );
+}
